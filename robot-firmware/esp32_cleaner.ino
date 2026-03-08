c94042390678a16fc4d3cb5a5b8e272f5c31c358
varshinigowda0731-hash <varshinigowda0731@gmail.com>
2026-03-08 23:05:26 +0530

    Update esp32_cleaner.ino

--- robot-firmware/esp32_cleaner.ino
+++ robot-firmware/esp32_cleaner.ino
@@ -1,1 +1,33 @@
+// Robotic Mopping Cleaner - ESP32 Firmware
 
+int motorLeft1 = 13;
+int motorLeft2 = 12;
+int motorRight1 = 14;
+int motorRight2 = 27;
+
+void setup() {
+
+  Serial.begin(115200);
+
+  pinMode(motorLeft1, OUTPUT);
+  pinMode(motorLeft2, OUTPUT);
+  pinMode(motorRight1, OUTPUT);
+  pinMode(motorRight2, OUTPUT);
+
+}
+
+void loop() {
+
+  moveForward();
+
+}
+
+void moveForward() {
+
+  digitalWrite(motorLeft1, HIGH);
+  digitalWrite(motorLeft2, LOW);
+
+  digitalWrite(motorRight1, HIGH);
+  digitalWrite(motorRight2, LOW);
+
+}
